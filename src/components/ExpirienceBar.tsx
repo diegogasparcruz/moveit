a7264ff39805b0e96c66694c415fb3461444d851
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-05-28 00:30:03 -0300

    style: create expirience bar module css

--- src/components/ExpirienceBar.tsx
+++ src/components/ExpirienceBar.tsx
@@ -1,11 +1,13 @@
+import styles from "../styles/components/ExpirienceBar.module.css";
+
 export function ExpirienceBar() {
   return (
-    <header className="expirience-bar">
+    <header className={styles.expirienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: "50%" }}></div>
 
-        <span className="current-expirience" style={{ left: "50%" }}>
+        <span className={styles.currentExpirience} style={{ left: "50%" }}>
           300 xp
         </span>
       </div>
