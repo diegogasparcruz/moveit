961c2c62683e860cc526bbadd972344a5a243e9f
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-05-28 00:54:32 -0300

    feat: add components styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExpirienceBar } from "../components/ExpirienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExpirienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+        </div>
+        <div></div>
+      </section>
     </div>
   );
 }
