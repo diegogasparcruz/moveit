d2b6e34fcc72f6ba9d85591f336257230003f0df
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-05-28 23:46:37 -0300

    feat: add challenge box component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
+import Head from "next/head";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExpirienceBar } from "../components/ExpirienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from "next/head";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -22,7 +22,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
